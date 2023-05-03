036d0eee9320791535ab7ae039501c795ecc8e02
Luke Donohue <ludon00@gmail.com>
2023-05-03 17:15:08 -0500

    Changes to LevelUpSystem Math

--- Assets/LevelUpTool/Scripts/PlayerLevelSystem.cs
+++ Assets/LevelUpTool/Scripts/PlayerLevelSystem.cs
@@ -6,23 +6,43 @@ using TMPro;
 
 public class PlayerLevelSystem : MonoBehaviour
 {
+
+    private float lerpTimer;
+    private float delayTimer;
+    
+    
     [Header("Level Information")]
     public int level;
     public float currentEXP;
     public float requiredEXP;
 
+    [Header("Level Up Math")]
+    [Range(1f,300f)]
+    [Tooltip("Adjusts a flat amount how much xp each level needs, this will have a minmal effect")]
+    public float additionMultiplier = 300;
+    [Range(2f, 4f)]
+    [Tooltip("If powerMultiplier is higher lower levels will be closer together in exp values. If lower xp values will be further apart")]
+    public float powerMultiplier = 2;
+    [Range(7f, 14f)]
+    [Tooltip("Lower divisionMultiplier makes the xp graph more of a bell curve, a higher value will make the graph more evenly distributed")]
+    public float divisionMultiplier = 7;
+
     [Header("UI")]
     [SerializeField] Image expBar;
+    [SerializeField] Image backexpBar;
     [SerializeField] TextMeshProUGUI LevelText;
 
     [Header("Effects")]
-    [SerializeField] AudioClip LevelUpAudio;
+    [SerializeField] AudioSource LevelUpAudio;
 
 
 
     private void Start()
     {
         expBar.fillAmount = currentEXP / requiredEXP;
+        backexpBar.fillAmount = currentEXP / requiredEXP;
+        requiredEXP = CalculateEXP();
+        LevelText.text = "Level " + level;
     }
 
     private void Update()
@@ -37,8 +57,18 @@ public class PlayerLevelSystem : MonoBehaviour
     private void UpdateUI() //Updates UI to reflect level on screen
     {
         float expFraction = currentEXP / requiredEXP;
-        expBar.fillAmount = expFraction;
-        LevelText.text = "Level " + level;
+        float FXP = expBar.fillAmount;
+        if(FXP < expFraction)
+        {
+            delayTimer += Time.deltaTime;
+            backexpBar.fillAmount = expFraction;
+            if(delayTimer > .5)
+            {
+                lerpTimer += Time.deltaTime;
+                float percentComplete = lerpTimer /4;
+                expBar.fillAmount = Mathf.Lerp(FXP, backexpBar.fillAmount, percentComplete);
+            }
+        }
     }
 
     public void GainExp(float amountGained)
@@ -49,7 +79,25 @@ public class PlayerLevelSystem : MonoBehaviour
     public void LevelUp() //Attactch level up logic here
     {
         level++;
+        LevelText.text = "Level " + level;
         expBar.fillAmount = 0f;
+        backexpBar.fillAmount = 0f;
         currentEXP = Mathf.RoundToInt(currentEXP - requiredEXP);
+        requiredEXP = CalculateEXP();
+
+        AudioSource newSound = Instantiate(LevelUpAudio);
+        Destroy(newSound.gameObject, newSound.clip.length);
+
+
+    }
+
+    private int CalculateEXP()
+    {
+        int solveForRequiredXp = 0;
+        for (int levelCycle = 1; levelCycle <= level; levelCycle++)
+        {
+            solveForRequiredXp += (int)Mathf.Floor(levelCycle + additionMultiplier * Mathf.Pow(powerMultiplier, levelCycle / divisionMultiplier));
+        }
+        return solveForRequiredXp / 4;
     }
 }
